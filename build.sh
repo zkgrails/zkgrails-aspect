2a4227d0f7aca82426ac15833de8635fb77607b6
Chanwit Kaewkasi <chanwit@gmail.com>
2010-06-30 21:22:44 +0700

    changed to 5.0.3

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-FILE=5.0.3-FL-2010-06-26
-wget http://downloads.sourceforge.net/project/zk1/ZK%20Freshly/zk-$FILE/zk-bin-$FILE.zip -c
+FILE=5.0.3
+## wget http://downloads.sourceforge.net/project/zk1/ZK%20Freshly/zk-$FILE/zk-bin-$FILE.zip -c
+## wget http://www.zkoss.org/download/freshly/zk-$FILE/zkee-bin-eval-$FILE.zip -c
 rm dist/zk.jar
 cd injar/.
 rm -Rf zk-bin-*
 cd ..
-unzip zk-bin-$FILE.zip zk-bin-$FILE/dist/lib/z*.jar -d injar/.
-gradle -DzkFilename=zk-bin-$FILE clean compileJava
+unzip zkee-bin-eval-$FILE.zip zkee-bin-eval-$FILE/dist/lib/z*.jar -d injar/.
+gradle -DzkFilename=zkee-bin-eval-$FILE clean compileJava
