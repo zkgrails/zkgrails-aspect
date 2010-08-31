a13a69642cb2f932f7ef4acea2eca90c515accf3
Chanwit Kaewkasi <chanwit@gmail.com>
2010-08-31 19:53:01 +0700

    changed removing zk-bin to zkee-bin

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ FILE=5.0.3
 ## wget http://www.zkoss.org/download/freshly/zk-$FILE/zkee-bin-eval-$FILE.zip -c
 rm dist/zk.jar
 cd injar/.
-rm -Rf zk-bin-*
+rm -Rf zkee-bin-*
 cd ..
 unzip zkee-bin-eval-$FILE.zip zkee-bin-eval-$FILE/dist/lib/z*.jar -d injar/.
 gradle -DzkFilename=zkee-bin-eval-$FILE clean compileJava
