45f7bf3a086022ac4541da7b4f8477086b07e936
Chanwit Kaewkasi <chanwit@gmail.com>
2010-10-20 02:55:31 +0700

    added gradle wrapper.

---
+++ build-ce.sh
@@ -0,0 +1,10 @@
+FILE=5.0.5-FL-2010-10-19
+wget http://downloads.sourceforge.net/project/zk1/ZK%20Freshly/zk-$FILE/zk-bin-$FILE.zip -c
+mkdir dist
+mkdir injar
+rm dist/zk.jar
+cd injar/.
+rm -Rf zk-bin-*
+cd ..
+unzip zk-bin-$FILE.zip zk-bin-$FILE/dist/lib/z*.jar -d injar/.
+./gradlew -DzkFilename=zk-bin-$FILE clean compileJava
--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
-FILE=5.0.4
+FILE=5.0.5-FL-2010-10-19
 ## wget http://downloads.sourceforge.net/project/zk1/ZK%20Freshly/zk-$FILE/zk-bin-$FILE.zip -c
-## wget http://www.zkoss.org/download/freshly/zk-$FILE/zkee-bin-eval-$FILE.zip -c
+wget http://www.zkoss.org/download/freshly/zk-$FILE/zkee-bin-eval-$FILE.zip -c
+mkdir dist
+mkdir injar
 rm dist/zk.jar
 cd injar/.
 rm -Rf zkee-bin-*
 cd ..
 unzip zkee-bin-eval-$FILE.zip zkee-bin-eval-$FILE/dist/lib/z*.jar -d injar/.
-gradle -DzkFilename=zkee-bin-eval-$FILE clean compileJava
+./gradlew -DzkFilename=zkee-bin-eval-$FILE clean compileJava
