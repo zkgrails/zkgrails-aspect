9939f987a432a02058722f963e124764fe8a2017
Chanwit Kaewkasi <chanwit@gmail.com>
2011-05-11 21:35:33 +0700

    prepare to release

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 FILE=$1
 ## wget http://downloads.sourceforge.net/project/zk1/ZK%20Freshly/zk-$FILE/zk-bin-$FILE.zip -c
-wget http://www.zkoss.org/download/freshly/zk-$FILE/zkee-bin-eval-$FILE.zip -c
+## wget http://www.zkoss.org/download/freshly/zk-$FILE/zkee-bin-eval-$FILE.zip -c
 
 #
 # a hack to make java dir exists
