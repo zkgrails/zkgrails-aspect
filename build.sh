a60da0fd2ffbc7661872ae8db11ad5002f7e2c72
Chanwit Kaewkasi <chanwit@gmail.com>
2011-08-11 20:24:20 +0700

    using Maven dependency to weave
    Change-Id: Ia9ae5ddad44e942dd3eb6ed40cd8201f2b98a38c

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-FILE=$1
-## wget http://downloads.sourceforge.net/project/zk1/ZK%20Freshly/zk-$FILE/zk-bin-$FILE.zip -c
-## wget http://www.zkoss.org/download/freshly/zk-$FILE/zkee-bin-eval-$FILE.zip -c
+VER=$1
+
+rm -Rf injar
+mkdir injar
+cd injar
+wget http://203.158.7.11/artifactory/repo/org/zkoss/zk/zk/$VER/zk-$VER.jar
+wget http://203.158.7.11/artifactory/repo/org/zkoss/zk/zul/$VER/zul-$VER.jar
+cd ..
 
 #
 # a hack to make java dir exists
@@ -13,18 +18,18 @@ mkdir injar
 rm dist/zk.jar
 rm dist/zul.jar
 
-cd injar/.
-rm -Rf zkee-bin-*
-cd ..
-unzip zkee-bin-eval-$FILE.zip zkee-bin-eval-$FILE/dist/lib/z*.jar -d injar/.
+# cd injar/.
+# rm -Rf zkee-bin-*
+# cd ..
+# unzip zkee-bin-eval-$FILE.zip zkee-bin-eval-$FILE/dist/lib/z*.jar -d injar/.
 
 cd zk
 mkdir -p src/main/java
-../gradlew -Dgoogle.code.username=$GOOGLE_CODE_ACC -Dgoogle.code.password=$GOOGLE_CODE_PWD -DzkFilename=zkee-bin-eval-$FILE clean uploadArchives
+../gradlew -Dgoogle.code.username=$GOOGLE_CODE_ACC -Dgoogle.code.password=$GOOGLE_CODE_PWD -DzkVersion=$VER clean uploadArchives
 cd ..
 
 cd zul
 mkdir -p src/main/java
-../gradlew -Dgoogle.code.username=$GOOGLE_CODE_ACC -Dgoogle.code.password=$GOOGLE_CODE_PWD -DzkFilename=zkee-bin-eval-$FILE clean uploadArchives
+../gradlew -Dgoogle.code.username=$GOOGLE_CODE_ACC -Dgoogle.code.password=$GOOGLE_CODE_PWD -DzkVersion=$VER clean uploadArchives
 cd ..
 
